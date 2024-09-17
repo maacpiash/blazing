a60ca25b86361fb374b0ab34c63f416f2efd0bd1
Ahad Chowdhury <ahad@maacpiash.com>
2024-09-17 15:22:58 +1000

    Make CalendarEvent.User required (mandatory)

--- src/Data/CalendarEvent.cs
+++ src/Data/CalendarEvent.cs
@@ -3,7 +3,7 @@ namespace Blazing.Data;
 public class CalendarEvent
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public ApplicationUser? User { get; set; }
+    public required ApplicationUser User { get; set; }
     public string Title { get; set; } = "";
     public DateTime Date { get; set; } = DateTime.Now;
     public string Location { get; set; } = "";
---
+++ src/Migrations/20240917051157_AddCalendarEventUserConstraint.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazing.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCalendarEventUserConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CalendarEvent_AspNetUsers_UserId",
+                table: "CalendarEvent");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "CalendarEvent",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CalendarEvent_AspNetUsers_UserId",
+                table: "CalendarEvent",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CalendarEvent_AspNetUsers_UserId",
+                table: "CalendarEvent");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "CalendarEvent",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CalendarEvent_AspNetUsers_UserId",
+                table: "CalendarEvent",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
