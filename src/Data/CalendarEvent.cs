54564be3fdbb67e7e945547169adc0b06dc20117
Ahad Chowdhury <ahad@maacpiash.com>
2024-09-19 23:52:50 +1000

    Add DTO and conversion methods for CalendarEvent

--- src/Data/CalendarEvent.cs
+++ src/Data/CalendarEvent.cs
@@ -8,4 +8,37 @@ public class CalendarEvent
     public DateTime Date { get; set; } = DateTime.Now;
     public string Location { get; set; } = "";
     public string Description { get; set; } = "";
+
+    internal void UpdateFromDto(CalendarEventDto dto)
+    {
+        Title = dto.Title;
+        Description = dto.Description;
+        Location = dto.Location;
+        Date = new DateTime(dto.Date, dto.Time);
+    }
+}
+
+internal class CalendarEventDto
+{
+    internal Guid? Id { get; set; }
+    internal string Title { get; set; } = "";
+    internal string Description { get; set; } = "";
+    internal string Location { get; set; } = "";
+    internal DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    internal TimeOnly Time { get; set; } = TimeOnly.FromDateTime(DateTime.Now);
+
+    internal CalendarEvent ToCalendarEvent(ApplicationUser user)
+    {
+        var calendarEvent = new CalendarEvent()
+        {
+            User = user
+        };
+
+        if (Id is not null && Id != Guid.Empty) calendarEvent.Id = (Guid)Id;
+        calendarEvent.Title = Title;
+        calendarEvent.Description = Description;
+        calendarEvent.Location = Location;
+        calendarEvent.Date = new DateTime(Date, Time);
+        return calendarEvent;
+    }
 }
